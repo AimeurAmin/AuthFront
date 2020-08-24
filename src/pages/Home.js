96a0e246d5b9711dafd6bf45bb9b4acf9c91f9bc
AimeurAmin <mohammedamin.aimeur@gmail.com>
2020-08-24 20:31:39 +0100

    header on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import Header from '../components/Header';
 
 const Home = () => (
     <motion.div exit={{}}>
         <h1>Home</h1>
-        <NavLink to='/login'><h3>Sign Up</h3></NavLink>
-        <NavLink to='/signup'><h3>Sign In</h3></NavLink>
+        <Header />
     </motion.div>
 )
 
