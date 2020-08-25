4ae353edde9f00f2719eecc5dbae7dcfc1798914
AimeurAmin <mohammedamin.aimeur@gmail.com>
2020-08-25 20:00:57 +0100

    some changes

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -18,15 +18,55 @@ const Signup = () => {
     }
 
     return (
-        <div>
+        <div style={{overflowX:'hidden'}}>
             <NavLink onClick={handleClose} to='/' class="page__close page__close_login"></NavLink>
             <motion.div 
                 className=' exit-signup'
                 exit={exitAnimation}
                 transition={transition}
             >
+                
                 <AuthLink link='/login'>Sign In</AuthLink>
             </motion.div>
+
+
+            <motion.div className='container-text-qst-auth'
+                exit={{opacity:0}}
+                transition={{duration:1}}
+            >
+                <motion.div
+                    exit={{
+                        transform:['transform:translateX(-100%)','translateX(0)']
+                    }}
+                    transition={{duration:1}}
+                >
+                    <h1>New Here?</h1>
+                    <p>Sign up and discover a great </p>
+                    <p>amount of new opportunities!</p>
+                </motion.div>
+            </motion.div>
+
+
+            {/* <motion.h1
+                    exit={{
+                            left: '110%'}}
+                    transition={{ease: "easeOut", duration:1
+                    }}
+                    style={{
+                        color: 'white',
+                        width: '100%',
+                        position: 'fixed',
+                        top: '45%',
+                        left: '85%',
+                        transform: 'translate(-50%, -100%)',
+                        fontSize: '2.125rem'
+                    }}
+                >
+                    <motion.h3
+                        exit={{opacity:0}}
+                        transition={{duration:.3}}
+                    >New Here?</motion.h3>
+            </motion.h1> */}
         </div>
 )}
 
